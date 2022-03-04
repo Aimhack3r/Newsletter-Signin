00d45986e263afc68fba56c0924171962499d564
Aimhack3r <91526218+Aimhack3r@users.noreply.github.com>
2022-03-04 10:44:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+});
+app.post("/", function (req, res) {
+    const fname = req.body.firstname;
+    const lname = req.body.lastname;
+    const email = req.body.email;
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fname,
+                LNAME: lname
+            }
+        }]
+
+    };
+    const jsondata = JSON.stringify(data);
+    const url = "https://us14.api.mailchimp.com/3.0/lists/ce38b75e54";
+    const options = {
+        method: "POST",
+        auth: "mayank:ce82bad7ae7ad470bcb6bb7aaa463525-us14"
+    }
+    const request = https.request(url, options, function (response) {
+
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html")
+
+        }
+        else{
+res.sendFile(__dirname+"/failure.html")
+        }
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    });
+    request.write(jsondata);
+    request.end();
+});
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server is running on port 3000")
+});
+
+// ce82bad7ae7ad470bcb6bb7aaa463525-us14
+
+// ce38b75e54
